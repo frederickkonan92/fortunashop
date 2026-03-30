0addd9936ee8460325f3ad828d852b9039d67f19
frederickkonan92 <frederickkonan92@gmail.com>
2026-03-30 07:45:37 +0200

    fix: ajout hostname Unsplash dans next/image config

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,10 @@ const nextConfig: NextConfig = {
         hostname: 'hmtxovrqvslhcsscjjdy.supabase.co',
         pathname: '/storage/v1/object/public/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
     ],
   },
 };
